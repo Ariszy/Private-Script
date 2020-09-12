a35f857a647cd73dd41c6db5f6436b80d28e82ed
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2020-09-12 09:31:00 +0800

    Add files via upload

--- momobdc.js
+++ momobdc.js
@@ -1,17 +1,18 @@
 /*
 墨墨背单词3.5.4版本
 
-重写：^https\:\/\/api\.maimemo\.com\/api\/v1/users/info
-手动阻止掉：https://api.maimemo.com/api/v1/system/check
-
+［rewrite］
+^https\:\/\/api\.maimemo\.com\/api\/v1/users/info url script-response-body https://raw.githubusercontent.com/ZhiYi-N/Loon/master/momobdc.js
+https://api.maimemo.com/api/v1/system/check url reject
+［mitm］
 hostname=api.maimemo.com
 
 */
 let obj = JSON.parse($response.body);
 
 
-obj.data["user"]["inf_level"] = 15;
-obj.data["user"]["level"] = 15;
+obj.data["user"]["inf_level"] = 99;
+obj.data["user"]["level"] = 99;
 obj.data["user"]["inf_words_limit"] = 999999;
 
 $done({body: JSON.stringify(obj)});
