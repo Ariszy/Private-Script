fc761b246d0069b952ad493d448daf9175035357
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2020-10-18 12:55:00 +0800

    Update mb.js

--- mb.js
+++ mb.js
@@ -4,6 +4,7 @@ http-response https:\/\/api2\.mubu\.com\/v3\/api\/user\/current_user script-path
 #quantumult-x
 https:\/\/api2\.mubu\.com\/v3\/api\/user\/current_user url script-response-body https://raw.githubusercontent.com/ZhiYi-N/Loon/master/mb.js
 #mitm=api2.mubu.com
+*/
 let obj = JSON.parse($response.body);
 obj.data["level"] = "2";
 $done({body: JSON.stringify(obj)});
