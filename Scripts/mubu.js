642253ff8471ac0cc0a56178d1673e95993e48ed
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2020-10-23 10:05:05 +0800

    Update mubu.js

--- Scripts/mubu.js
+++ Scripts/mubu.js
@@ -1,1 +1,10 @@
-
+/*
+#loon
+http-response https:\/\/api2\.mubu\.com\/v3\/api\/user\/current_user script-path=https://raw.githubusercontent.com/ZhiYi-N/Loon/master/mb.js, requires-body=true, timeout=10, tag=幕布
+#quantumult-x
+https:\/\/api2\.mubu\.com\/v3\/api\/user\/current_user url script-response-body https://raw.githubusercontent.com/ZhiYi-N/Loon/master/mb.js
+#mitm=api2.mubu.com
+*/
+let obj = JSON.parse($response.body);
+obj.data["level"] = "2";
+$done({body: JSON.stringify(obj)});
