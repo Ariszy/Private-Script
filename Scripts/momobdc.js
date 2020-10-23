b13a6695435ffbabc90887f9357065615c3f98ff
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2020-10-23 12:58:00 +0800

    Update momobdc.js

--- Scripts/momobdc.js
+++ Scripts/momobdc.js
@@ -12,10 +12,7 @@ https://api.maimemo.com/api/v1/system/check _ reject
 hostname=api.maimemo.com
 */
 let obj = JSON.parse($response.body);
-
-
 obj.data["user"]["inf_level"] = 99;
 obj.data["user"]["level"] = 99;
 obj.data["user"]["inf_words_limit"] = 999999;
-
 $done({body: JSON.stringify(obj)});
