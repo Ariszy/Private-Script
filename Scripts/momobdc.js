eda86fe5ee5933eaa75b8b0b15bf669742a414ee
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2020-10-23 13:01:52 +0800

    Update momobdc.js

--- Scripts/momobdc.js
+++ Scripts/momobdc.js
@@ -2,7 +2,7 @@
 墨墨背单词3.5.4版本
 #圈x
 ［rewrite］
-^https\:\/\/api\.maimemo\.com\/api\/v1/users/info url script-response-body https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/momobdcjs
+^https\:\/\/api\.maimemo\.com\/api\/v1/users/info url script-response-body https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/momobdc.js
 https://api.maimemo.com/api/v1/system/check url reject
 #Loon
 http-response ^https\:\/\/api\.maimemo\.com\/api\/v1/users/info script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/momobdc.js, requires-body=true, timeout=10, enabled=false, tag=默默背单词
