064c75fa020a8c805b522c943c436e8dea6bed57
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2021-01-14 21:35:23 +0800

    Update xp.js

--- Scripts/xp.js
+++ Scripts/xp.js
@@ -164,9 +164,9 @@ if($request.body.indexOf('isFinishWatch')&& $request.body.indexOf('"type":2')>=0
  }
  }
 async function control(){
-   if(coins >= 1 && hour == 0){
+  /* if(coins >= 1 && hour == 0){
       await withdraw();
-}
+}*/
    if(goldbody && gold == 1){
       await watch_goldvideo();
    }else{
