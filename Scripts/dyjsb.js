adc6615c28ff57e6e02be380775edf02239ee4ee
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2021-02-05 20:18:57 +0800

    更改接口，请重新添加重写，添加自动提现0.3，请更新boxjs并填写支付宝账号
    exist bugs, possibly

--- Scripts/dyjsb.js
+++ Scripts/dyjsb.js
@@ -1,6 +1,4 @@
 /*
-tgchannel：https://t.me/ZhiYi_Script
-github action有问题，看视频金币太少，建议使用代理软件
 github：https://github.com/ZhiYi-N/script
 boxjs：https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/ZhiYi-N.boxjs.json
 转载留个名字，谢谢
@@ -16,32 +14,30 @@ boxjs：https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/ZhiYi-N.
 #走路修改步数，提前之前需要重新获取ck，不然提交失败，进一次任务界面就可
 #看一个视频弹出金币获取readheader and readkey
 [mitm]
-hostname = api5-normal-c-*.amemv.com
-
+hostname = api5-normal-lite-act-*.amemv.com
 #圈x
 [rewrite local]
-https://api5-normal-c-\w+.amemv.com/luckycat/aweme/v1/task/sign_in/detail? url script-request-header https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js
+https://api5-normal-lite-act-.*.amemv.com/luckycat/aweme/v1/task/sign_in/detail? url script-request-header https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js
 
-https://api5-normal-c-\w+.amemv.com/luckycat/aweme/v1/task/done/read? url script-request-header https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js
+https://api5-normal-lite-act-.*.amemv.com/luckycat/aweme/v1/task/done/read? url script-request-header https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js
 
-https://api5-normal-c-\w+.amemv.com/luckycat/aweme/v1/task/walk/step_submit? - script-request-header https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js
+https://api5-normal-lite-act-.*.amemv.com/luckycat/aweme/v1/task/walk/step_submit? - script-request-header https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js
 
 #loon
-http-request ^https://api5-normal-c-\w+.amemv.com/luckycat/aweme/v1/task/sign_in/detail? script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js, requires-body=true, timeout=10, tag=抖音极速版sign
+http-request ^https://api5-normal-lite-act-.*.amemv.com/luckycat/aweme/v1/task/sign_in/detail? script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js, requires-body=true, timeout=10, tag=抖音极速版sign
 
-http-request ^https://api5-normal-c-\w+.amemv.com/luckycat/aweme/v1/task/done/read? script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js, requires-body=true, timeout=10, tag=抖音极速版read
+http-request ^https://api5-normal-lite-act-.*.amemv.com/luckycat/aweme/v1/task/done/read? script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js, requires-body=true, timeout=10, tag=抖音极速版read
 
-http-request ^https://api5-normal-c-\w+.amemv.com/luckycat/aweme/v1/task/walk/step_submit? script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js, requires-body=true, timeout=10, tag=抖音极速版step
+http-request ^https://api5-normal-lite-act-.*.amemv.com/luckycat/aweme/v1/task/walk/step_submit? script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js, requires-body=true, timeout=10, tag=抖音极速版step
 
 #surge
-dyjsbsign = type=http-request,pattern=^https://api5-normal-c-\w+.amemv.com/luckycat/aweme/v1/task/sign_in/detail?,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js,script-update-interval=0
+dyjsbsign = type=http-request,pattern=^https://api5-normal-lite-act-.*.amemv.com/luckycat/aweme/v1/task/sign_in/detail?,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js,script-update-interval=0
 
-dyjsbread = type=http-request,pattern=^https://api5-normal-c-\w+.amemv.com/luckycat/aweme/v1/task/done/read?,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js,script-update-interval=0
+dyjsbread = type=http-request,pattern=^https://api5-normal-lite-act-.*.amemv.com/luckycat/aweme/v1/task/done/read?,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js,script-update-interval=0
 
-dyjsbstep = type=http-request,pattern=^https://api5-normal-c-\w+.amemv.com/luckycat/aweme/v1/task/walk/step_submit?,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js,script-update-interval=0
+dyjsbstep = type=http-request,pattern=^https://api5-normal-lite-act-.*.amemv.com/luckycat/aweme/v1/task/walk/step_submit?,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/dyjsb.js,script-update-interval=0
 
 */
-
 const jsname='抖音极速版'
 const $ = Env(jsname)
 const notify = $.isNode() ?require('./sendNotify') : '';
@@ -57,9 +53,10 @@ let stepkey = $.getdata('stepkey')
 
 let readheader = $.getdata('readheader')
 let readkey = $.getdata('readkey')
+let dyjsbaccount = ($.getval('dyjsbaccount') || '0')
 let tz = ($.getval('tz') || '1');//0关闭通知，1默认开启
 const invite=1;//新用户自动邀请，0关闭，1默认开启
-const logs =1;//0为关闭日志，1为开启
+const logs =0;//0为关闭日志，1为开启
 var hour=''
 var minute=''
 const readbody = `{
@@ -209,10 +206,11 @@ if (!signheaderArr[0]) {
       console.log(`\n开始【抖音极速版${$.index}】`)
       //await invite()
       await sign_in()
-      //await step_submit()
+      //await withdraw()
       //await step_reward()
       await watch_video()
       await control()
+      //await profit()
       await showmsg()
   }
  }
@@ -259,12 +257,15 @@ async function control(){
      if(invite == 1){
       await invitation();
      }
+     if(dyjsbaccount){
+      await profit()
+     }
 }
 //签到
 function sign_in() {
 return new Promise((resolve, reject) => {
   let sign_inurl ={
-    url: `https://aweme-lq.snssdk.com/luckycat/aweme/v1/task/done/sign_in?${signheader}`,
+    url: `https://api5-normal-lite-act-lq.amemv.com/luckycat/aweme/v1/task/done/sign_in?${signheader}`,
     headers :{
     	Cookie: signcookie,
     	'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 12_4_1 like Mac OS X) AppleWebKit/605.1.12 (KHTNL, like Gecko) Mobile/15E148'
@@ -289,7 +290,7 @@ const steps = Math.round(Math.random()*(12000 - 10001) + 10001);
 const time = Math.round(new Date().getTime()/1000).toString();
 return new Promise((resolve, reject) => {
   let step_submiturl ={
-	url: `https://aweme-lq.snssdk.com/luckycat/aweme/v1/task/walk/step_submit?${stepheader}`,
+	url: `https://api5-normal-lite-act-lq.amemv.com/luckycat/aweme/v1/task/walk/step_submit?${stepheader}`,
     headers: JSON.parse(stepkey),
     body:`{
   "step" : ${steps},
@@ -315,7 +316,7 @@ return new Promise((resolve, reject) => {
 function step_reward() {
 return new Promise((resolve, reject) => {
   let step_rewardurl ={
-      url: `https://aweme-lq.snssdk.com/luckycat/aweme/v1/task/walk/receive_step_reward?${stepheader}`,
+      url: `https://api5-normal-lite-act-lq.amemv.com/luckycat/aweme/v1/task/walk/receive_step_reward?${stepheader}`,
       headers: JSON.parse(stepkey),
 	  body:`{"in_sp_time":0}`
 }
@@ -336,7 +337,7 @@ return new Promise((resolve, reject) => {
 function watch_video() {
 return new Promise((resolve, reject) => {
   let watch_videourl ={
-    url: `https://aweme-lq.snssdk.com/luckycat/aweme/v1/task/done/read?${readheader}`,
+    url: `https://api5-normal-lite-act-lq.amemv.com/luckycat/aweme/v1/task/done/read?${readheader}`,
     headers: JSON.parse(readkey),
     body: `{
   "in_sp_time" : 0,
@@ -365,7 +366,7 @@ return new Promise((resolve, reject) => {
 function invitation() {
 return new Promise((resolve, reject) => {
   let invitatonurl ={
-    url: `https://aweme-lq.snssdk.com/luckycat/aweme/v1/task/done/post_invite_code?${signheader}`,
+    url: `https://api5-normal-lite-act-lq.amemv.com/luckycat/aweme/v1/task/done/post_invite_code?${signheader}`,
     headers: JSON.parse(readkey),
     body: JSON.stringify({"in_sp_time":0,"invite_code":"8025524531"})
 }
@@ -376,7 +377,54 @@ return new Promise((resolve, reject) => {
     })
    })
   } 
+//profit page
+function profit() {
+return new Promise((resolve, reject) => {
+  let profiturl ={
+    url: `https://api5-normal-lite-act-lq.amemv.com/luckycat/aweme/v1/wallet/profit_detail_page?income_type=1&offset=0&num=50&share_page=profits_detail_page&key=coin&${stepheader}`,
+    headers: JSON.parse(readkey),
+}
+   $.get(profiturl,async(error, response, data) =>{
+     const result = JSON.parse(data)
+     if(logs) $.log(data)
+     let time = Math.round(new Date(new Date().toLocaleDateString()).getTime()/1000)
+if(!result.data.profit_detail.cash_income_list.find(item => item.time >= time) && !result.data.profit_detail.cash_income_list.find(item => item.task_id == "213") && result.data.income_data.cash_balance >= 30){
+     await withdraw()
+     }
+          resolve()
+    })
+   })
+  } 
+//withdraw alipay 0.3
+function withdraw() {
+return new Promise((resolve, reject) => {
+  let withdrawurl ={
+    url: `https://api5-normal-lite-act-lq.amemv.com/luckycat/aweme/v1/wallet/take_cash?task_key=jiao_take_cash&${signheader}`,
+    headers: JSON.parse(readkey),
+    body: `{
+  "account" : "${dyjsbaccount}",
+  "is_auto" : false,
+  "take_cash_type" : 3,
+  "tab_type" : "alipay",
+  "in_sp_time" : 0,
+  "cash_amount" : -30,
+  "name" : ""
+}`
+}
 
+   $.post(withdrawurl,(error, response, data) =>{
+     const result = JSON.parse(data)
+     if(logs) $.log(data)
+     if(result.err_no == 0){
+     console.log(result.err_tips+'提现0.3元\n')
+     message += result.err_tips+'提现0.3元\n'
+     }else{
+     console.log(result.err_tips)
+     }
+          resolve()
+    })
+   })
+  }
 async function showmsg(){
 if(tz==1){
     if ($.isNode()){
@@ -392,4 +440,5 @@ if(tz==1){
        $.log(message)
     }
  }
+
 function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?(this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)})):this.isQuanX()?(this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t))):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)}))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)})}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r)));let h=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];h.push(e),s&&h.push(s),i&&h.push(i),console.log(h.join("\n")),this.logs=this.logs.concat(h)}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
