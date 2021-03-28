74fe138c6b883dd87b3079e4196a21812fec9a57
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2021-03-28 11:59:24 +0800

    Update fqxs.js

--- Scripts/fqxs.js
+++ Scripts/fqxs.js
@@ -32,7 +32,7 @@ status = (status = ($.getval("fqxsstatus") || "1") ) > 1 ? `${status}` : ""; // 
 const fqxsurlArr = [],fqxsArr = []
 let fqxaurl = $.getdata('fqxsurl')
 let fqxs= $.getdata('fqxs')
-let host = $.getdata('fqxshost')
+let host = $.getdata('host')
 let tz = ($.getval('tz') || '1');//0关闭通知，1默认开启
 const invite=1;//新用户自动邀请，0关闭，1默认开启
 const logs =0;//0为关闭日志，1为开启
