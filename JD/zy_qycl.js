24e5a6533ec9bf692baa1c28c241d591cd0bda87
Ariszy <34622776+Ariszy@users.noreply.github.com>
2021-09-14 15:20:23 +0800

    Update zy_qycl.js

--- JD/zy_qycl.js
+++ JD/zy_qycl.js
@@ -4,15 +4,15 @@ github：https://github.com/Ariszy/Private-Script
 boxjs：https://raw.githubusercontent.com/Ariszy/Private-Script/master/Ariszy.boxjs.json
 
 [task_local]
-#开学充电站
-30 1 * * * https://raw.githubusercontent.com/Ariszy/Private-Script/master/JD/zy_kxcdz.js, tag= 开学充电站
+#企有此礼
+30 0 * * * https://raw.githubusercontent.com/Ariszy/Private-Script/master/JD/zy_qycl.js, tag= 企有此礼
 ================Loon==============
 [Script]
-cron "30 1 * * *" script-path= https://raw.githubusercontent.com/Ariszy/Private-Script/master/JD/zy_kxcdz.js,tag= 开学充电站
+cron "30 0 * * *" script-path= https://raw.githubusercontent.com/Ariszy/Private-Script/master/JD/zy_qycl.js,tag= 企有此礼
 ===============Surge=================
-开学充电站 = type=cron,cronexp="30 1 * * *",wake-system=1,timeout=3600,script-path= https://raw.githubusercontent.com/Ariszy/Private-Script/master/JD/zy_kxcdz.js
+企有此礼 = type=cron,cronexp="30 0 * * *",wake-system=1,timeout=3600,script-path= https://raw.githubusercontent.com/Ariszy/Private-Script/master/JD/zy_qycl.js
 ============小火箭=========
-开学充电站 = type=cron,script-path= https://raw.githubusercontent.com/Ariszy/Private-Script/master/JD/zy_kxcdz.js, cronexpr="30 1 * * *", timeout=3600, enable=true
+企有此礼 = type=cron,script-path= https://raw.githubusercontent.com/Ariszy/Private-Script/master/JD/zy_qycl.js, cronexpr="30 0 * * *", timeout=3600, enable=true
 */
 const $ = new Env('企有此礼')
 const notify = $.isNode() ?require('./sendNotify') : '';
